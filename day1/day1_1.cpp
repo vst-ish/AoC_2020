1c6105c49d5d8f1cc4123ced1ed46eabad64d08e
vst <60248247+vst-ish@users.noreply.github.com>
2020-12-15 12:42:48 -0500

    day1 parts 1 and 2

---
+++ day1/day1_1.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    if (argc !=2)
+    {
+        std::cout << "Need input file." << std::endl;
+        return 0;
+    }
+    std::vector<int> expenses;
+
+    std::ifstream ist(argv[1]);
+    if(!ist) std::cout<<"Unable to open file." << std::endl;
+    
+    for(int i=0; ist>>i;)
+    {
+        expenses.push_back(i);
+    }
+    std::cout<<"numbers in vector: "<< expenses.size()<<std::endl;
+
+    int baseNumber = 0;
+    int otherNumber = 0;
+
+    for(int i=0; i<expenses.size();i++)
+    {
+        baseNumber = expenses[i];
+
+        for(int j=0; j<expenses.size();j++)
+        {
+            otherNumber = expenses[j];
+
+            std::cout<<"Total: " << baseNumber + otherNumber << std::endl;
+
+            if (baseNumber + otherNumber == 2020)
+            {
+                std::cout << "The answer is: " << baseNumber * otherNumber << std::endl;
+                return 0;
+            }
+        }
+    }
+
+    std::cout<<"COMPLETE."<<std::endl;
+
+    return 0;
+} //end of main
---
+++ day1/day1_2.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+    if (argc !=2)
+    {
+        std::cout << "Need input file." << std::endl;
+        return 0;
+    }
+    std::vector<int> expenses;
+
+    std::ifstream ist(argv[1]);
+    if(!ist) std::cout<<"Unable to open file." << std::endl;
+    
+    for(int i=0; ist>>i;)
+    {
+        expenses.push_back(i);
+    }
+
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int thirdNumber = 0;
+
+    for(int i=0; i<expenses.size();i++)
+    {
+        firstNumber = expenses[i];
+
+        for(int j=0; j<expenses.size();j++)
+        {
+            secondNumber = expenses[j];
+
+            for (int k=0; k<expenses.size(); k++)
+            {
+                thirdNumber = expenses[k];
+
+                if (firstNumber + secondNumber + thirdNumber == 2020)
+                {
+                    std::cout << "The answer is: " << firstNumber * secondNumber *thirdNumber << std::endl;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    std::cout<<"COMPLETE."<<std::endl;
+
+    return 0;
+} //end of main
